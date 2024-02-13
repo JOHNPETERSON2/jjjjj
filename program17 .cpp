a1a0f6c82c1f3401f9fc698e41553bcf9b5d7930
JOHNPETERSON2 <158233132+JOHNPETERSON2@users.noreply.github.com>
2024-02-14 07:23:19 +0800

    Update program17 .cpp

--- program17 .cpp
+++ program17 .cpp
@@ -5,7 +5,7 @@ int main() {
 	char input;
 	cout<<"Enter a character: ";
 	cin>>input;
-	if((input >= 'a' && input <= '2')||(input >= 'A' && input <= 'Z'))
+	if((input >= 'a' && input <= 'Z')||(input >= 'A' && input <= 'Z'))
 	   cout<<"Your have entered a character.\n";
 	else
 	   cout<<"Invalid input.\n";
